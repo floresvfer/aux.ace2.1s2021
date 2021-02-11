01636ea683d354c469913394e8e8a7dafd22e503
Fernando Flores <nbafernandoflores8@gmail.com>
2021-02-11 16:09:06 -0600

    database conection add

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const app = express()
 const port =  process.env.PORT || 80
+var sequelize = require('./products-api_conn.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+
+
+assertDatabaseConnectionOk()
+    .then(() => {
+      console.log('syncing...')
+      return sequelize.sync({ alter: true });
+    })
+    .then(() => {
+      console.log('database ok.')
+      /*server.listen(port);
+      server.on('error', onError);
+      server.on('listening', onListening);*/
+        app.listen(port, () => {
+          console.log(`Example app listening at http://localhost:${port}`)
+        });
+    });
+
+
+async function assertDatabaseConnectionOk() {
+  console.log(`Checking database connection...`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection OK!');
+  } catch (error) {
+    console.log('Unable to connect to the database:');
+    console.log(error.message);
+    process.exit(1);
+  }
+}
