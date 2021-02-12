bec81d5edd356d029ae4d15b75b448c76956abbc
Fernando Flores <nbafernandoflores8@gmail.com>
2021-02-12 08:36:38 -0600

    users entpoint add

---
+++ api-routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { models } = require('../conn');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await models.user.findAll();
+        res.status(200).json(users);
+    } catch (e){
+        console.error(e);
+        res.status(500).json({message: e.message});
+    }
+});
