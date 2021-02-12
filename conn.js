4b6fc123a58a9697edc3ab6efd61798480393ad7
Fernando Flores <nbafernandoflores8@gmail.com>
2021-02-12 08:15:33 -0600

    user model add

--- conn.js
+++ conn.js
@@ -30,4 +30,21 @@ sequelize.define('product', {
     }
 });
 
+sequelize.define('user', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    name: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    pwd: {
+        allowNull: false,
+        type: DataTypes.STRING
+    }
+});
+
 module.exports = sequelize;
