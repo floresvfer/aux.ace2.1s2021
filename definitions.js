833ac1cb5fa6c37a329cb95dc63d1575871d0bbc
Fernando Flores <nbafernandoflores8@gmail.com>
2021-02-11 19:34:49 -0600

    definitions refactor

--- definitions.js
+++ definitions.js
@@ -1,26 +1,10 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-
-// DATABASE_URL=postgres://{user}:{password}@{hostname}:{port}/{database-name}
-
-//const user = 'postgres';
-/*const password = 'admin';
-const hostname = 'postgres-container';
-const port = 5432;
-const database = 'postgres';
-
-const DATABASE_URL = `postgres://${user}:${password}@${hostname}:${port}/${database}`
-console.log('DB URL: ', DATABASE_URL);*/
-
-// antiguo
+//DB Conn.
 const DATABASE_URL = 'postgres://nqtrbemn:3Y9Git59-UCSrTLMVYkUnsw9tBW7p6bY@ziggy.db.elephantsql.com:5432/nqtrbemn';
-
-// Conexion a la base de datos
 const sequelize = new Sequelize(DATABASE_URL);
 
 sequelize.define('product', {
-    // The following specification of the 'id' attribute could be omitted
-    // since it is the default.
     id: {
         allowNull: false,
         autoIncrement: true,
